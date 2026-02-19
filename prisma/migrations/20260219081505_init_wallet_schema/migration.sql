6d890df0145900dc2e69012dc4d98689a92b3ca0
Tharun-77 <tharunthej.reddy8@gmail.com>
2026-02-19 08:42:51 +0000

    made prisma for structred sql schema maintaining and type safety

---
+++ prisma/migrations/20260219081505_init_wallet_schema/migration.sql
@@ -0,0 +1,105 @@
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('TOPUP', 'BONUS', 'SPEND');
+
+-- CreateEnum
+CREATE TYPE "TransactionStatus" AS ENUM ('PENDING', 'SUCCESS', 'FAILED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "assets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "assets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "wallets" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "assetId" TEXT NOT NULL,
+    "balance" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "wallets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "transactions" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" "TransactionType" NOT NULL,
+    "status" "TransactionStatus" NOT NULL DEFAULT 'PENDING',
+    "idempotencyKey" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ledger_entries" (
+    "id" TEXT NOT NULL,
+    "walletId" TEXT NOT NULL,
+    "transactionId" TEXT NOT NULL,
+    "amount" DECIMAL(65,30) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ledger_entries_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "assets_name_key" ON "assets"("name");
+
+-- CreateIndex
+CREATE INDEX "wallets_userId_idx" ON "wallets"("userId");
+
+-- CreateIndex
+CREATE INDEX "wallets_assetId_idx" ON "wallets"("assetId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "wallets_userId_assetId_key" ON "wallets"("userId", "assetId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "transactions_idempotencyKey_key" ON "transactions"("idempotencyKey");
+
+-- CreateIndex
+CREATE INDEX "transactions_userId_idx" ON "transactions"("userId");
+
+-- CreateIndex
+CREATE INDEX "transactions_userId_createdAt_idx" ON "transactions"("userId", "createdAt");
+
+-- CreateIndex
+CREATE INDEX "ledger_entries_walletId_idx" ON "ledger_entries"("walletId");
+
+-- CreateIndex
+CREATE INDEX "ledger_entries_transactionId_idx" ON "ledger_entries"("transactionId");
+
+-- AddForeignKey
+ALTER TABLE "wallets" ADD CONSTRAINT "wallets_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "wallets" ADD CONSTRAINT "wallets_assetId_fkey" FOREIGN KEY ("assetId") REFERENCES "assets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ledger_entries" ADD CONSTRAINT "ledger_entries_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "wallets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ledger_entries" ADD CONSTRAINT "ledger_entries_transactionId_fkey" FOREIGN KEY ("transactionId") REFERENCES "transactions"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
+//for security purposes, we are disabling the content security policy as it can interfere with some of the features of our application. In a production environment, you should configure the content security policy according to your application's needs.
+app.use(
+    helmet({
+        contentSecurityPolicy: false, 
+    })
+);
+// Middleware
+app.use(express.json());
+// Enable CORS for all routes
+app.use(cors());
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
